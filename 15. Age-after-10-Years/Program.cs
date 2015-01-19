08fdb8cb12c56b50fee3ad9d52479d7a4501b50b
Peter-Alexandrov <peter.alexandrov@mail.bg>
2015-01-19 20:44:25 +0200

    Add homework 1

---
+++ 15. Age-after-10-Years/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age:");
+        int Age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Your age after 10 years will be:{0}", Age + 10);
+    }
+}
---
+++ 16. Print-Long-Sequence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class PrintLongSequence
+{
+
+    static void Main()
+    {
+
+        for (int i = 2; i < 1002; i++)
+        {
+
+            double y = Math.Pow(-1, i);
+
+            Console.Write(i * y);
+
+            Console.Write(',');
+
+        }
+
+    }
+
+}
+
